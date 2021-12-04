0e29d8d59f26a9fc9b455b8a24bcbf20b11fe259
Andrew Yatsutko <eitherwiser@gmail.com>
2021-12-04 16:23:00 +0200

    fix reload on submit

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -35,23 +35,30 @@ const ContactForm = () => {
     }
   };
 
-  const onSubmit = (e) => {
-    if (contacts.some(contact => contact.name === name)) {
-      alert(`"${name}" is already in your phonebook.`)
-      return;
-    } else if (contacts.some(contact => contact.number === number)) {
-      alert(`Person with number "${number}" is already in your phonebook.`)
-      return;
-    }
-      else if (contacts.some(contact => contact.email === email)) {
-      alert(`Person with E-mail "${email}" is already in your phonebook.`)
-      return;
-    }
-    dispatch(addContact({ name, number, email }))
+  const resetForm = (e) => {
+    e.preventDefault();
     setName('');
     setNumber('');
     setEmail('');
-    e.preventDefault();
+  }
+
+  const onSubmit = (e) => {
+    if (contacts.some(contact => contact.name === name)) {
+      resetForm(e);
+      return alert(`"${name}" is already in your phonebook.`);
+    }
+    else if (contacts.some(contact => contact.number === number)) {
+      resetForm(e);
+      return alert(`Person with number "${number}" is already in your phonebook.`);
+    }
+    else if (contacts.some(contact => contact.email === email)) {
+      resetForm(e);
+      return alert(`Person with E-mail "${email}" is already in your phonebook.`);
+    }
+    else {
+      dispatch(addContact({ name, number, email }));
+      resetForm(e);
+    }    
   };
 
   return (
